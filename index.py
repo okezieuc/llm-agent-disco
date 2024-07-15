35cb16bae52bfc11955d70215c706907533463d5
Okezie Chiedozie <53785400+okezieuc@users.noreply.github.com>
2024-07-15 05:15:17 +0000

    add support for tool calling
    agent is now able to use tools to produce responses to prompts.

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
+import json
 import os
+import re
 from dotenv import load_dotenv
 from openai import OpenAI
 import prompt_parts.system_prompt_parts
@@ -12,7 +14,7 @@ client = OpenAI(
 
 
 def sum(args):
-    return args.x + args.y
+    return args["x"] + args["y"]
 
 
 tools = [
@@ -30,24 +32,60 @@ tool_implementations = {
     "sum": sum,
 }
 
+
+def parse_tool_call(response):
+    pattern = r"\bAction \d+:\b"
+    tool_call = re.split(pattern, response)[-1]
+    # search for the boundaries of the returned JSON
+    k = tool_call.find("{")
+    l = tool_call.rfind("}")
+    tool_call = tool_call[k : l + 1]
+    return json.loads(tool_call)
+
 prompt = input("Enter a prompt: ")
 
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": prompt_parts.system_prompt_parts.prompt_intro},
-        {
-            "role": "system",
-            "content": prompt_parts.system_prompt_parts.sample_react_thought_chain,
-        },
-        {
-            "role": "system",
-            "content": prompt_parts.system_prompt_parts.tool_setup_intro,
-        },
-        {"role": "system", "content": str(tools)},
-        {"role": "system", "content": prompt_parts.system_prompt_parts.react_nudge},
-        {"role": "user", "content": "Question: " + prompt},
-    ],
-)
+messages = [
+    {"role": "system", "content": prompt_parts.system_prompt_parts.prompt_intro},
+    {
+        "role": "system",
+        "content": prompt_parts.system_prompt_parts.sample_react_thought_chain,
+    },
+    {
+        "role": "system",
+        "content": prompt_parts.system_prompt_parts.tool_setup_intro,
+    },
+    {"role": "system", "content": str(tools)},
+    {"role": "system", "content": prompt_parts.system_prompt_parts.react_nudge},
+    {"role": "user", "content": "Question: " + prompt},
+]
 
-print(completion.choices[0].message.content)
+iteration = 1
+while True:
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo", messages=messages
+    )
+
+    response = completion.choices[0].message.content
+    ## add the response to context
+    messages.append(
+        {"role": "assistant", "content": response}
+    )
+
+    tool_call = parse_tool_call(response)
+    if tool_call["tool"] == "finish":
+        print(tool_call["args"]["response"])
+        break
+
+    tool_name = tool_call["tool"]
+    tool_args = tool_call["args"]
+    tool_response = str(tool_implementations[tool_name](tool_args))
+    messages.append(
+        {
+            "role": "assistant",
+            "content": "Observation " + str(iteration) + ": " + tool_response,
+        }
+    )
+
+    iteration += 1
+    if iteration == 5:
+        break
